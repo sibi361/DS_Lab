003f6159a9d1fe7b0cb5befa2cd6af89f45c1b09
sibi361 <104310887+sibi361@users.noreply.github.com>
2023-08-13 18:56:57 +0530

    Add Subtract TIme

--- lab_2_strings/3_struct_time.c
+++ lab_2_strings/3_struct_time.c
@@ -6,12 +6,10 @@ struct Time
     int hour, min, sec;
 };
 
-struct Time read_time()
+struct Time read_time(int hour, int min, int sec)
 {
     struct Time t;
-    int valid = 0, hour, min, sec;
-
-    scanf("%d %d %d", &hour, &min, &sec);
+    int valid = 0;
 
     if (hour < 0 || hour > 23)
         printf("Invalid hour\n");
@@ -55,16 +53,37 @@ struct Time add_time(struct Time t1, struct Time t2)
     return t_new;
 };
 
+struct Time subtract_time(struct Time t1, struct Time t2)
+{
+    int sec_1, sec_2, sec_diff, h, m, s;
+
+    sec_1 = t1.hour * 3600 + t1.min * 60 + t1.sec;
+    sec_2 = t2.hour * 3600 + t2.min * 60 + t2.sec;
+
+    sec_diff = sec_1 - sec_2;
+
+    h = sec_diff / 3600;
+    m = sec_diff % 3600 / 60;
+    s = sec_diff % 3600 - m * 60;
+
+    struct Time t_new = {h, m, s};
+    return t_new;
+};
+
 int main()
 {
+    int valid = 0, hour, minute, second;
+
     printf("Enter time as <hour> <min> <second>:\n");
     // printf("Enter time 1: ");
-    // struct Time user_time_1 = read_time();
+    // scanf("%d %d %d", &hour, &minute, &second);
+    // struct Time user_time_1 = read_time(hour, minute, second);
     // printf("Enter time 2: ");
-    // struct Time user_time_2 = read_time();
+    // scanf("%d %d %d", &hour, &minute, &second);
+    // struct Time user_time_2 = read_time(hour, minute, second);
 
-    struct Time user_time_1 = {23, 59, 59};
-    struct Time user_time_2 = {0, 0, 3};
+    struct Time user_time_1 = read_time(10, 30, 15);
+    struct Time user_time_2 = read_time(2, 5, 10);
 
     printf("\nTime 1: ");
     print_time(user_time_1);
@@ -74,6 +93,10 @@ int main()
     struct Time added_time = add_time(user_time_1, user_time_2);
     printf("\nTime 1 + Time 2: ");
     print_time(added_time);
+
+    struct Time sub_time = subtract_time(user_time_1, user_time_2);
+    printf("\nTime 1 - Time 2: ");
+    print_time(sub_time);
 
     printf("\n");
 }
