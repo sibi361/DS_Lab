b54a18ee7c707ce9398e2714de3870ea63b18c23
sibi361 <104310887+sibi361@users.noreply.github.com>
2023-08-23 17:24:11 +0530

    Update `2_stack_base_converter.c`
    Turns out all base changes from 2 till 36 can be performed by a single
    algorithm that stores the individual characters in a character stack

--- lab_3_stacks/2_stack_base_converter.c
+++ lab_3_stacks/2_stack_base_converter.c
@@ -1,92 +1,62 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define MAX_SIZE 100
-
-int push(int stack[], int *top, int item);
-int pop(int stack[], int *top);
+int push(char stack[], int *top, int item);
+int pop(char stack[], int *top);
 int num_strlen(int input);
-void decimal_to_base_less_than_10(int stack[], int *top, int base, unsigned input);
-void decimal_to_octal(int stack[], int *top, unsigned input);
-void decimal_to_hexadecimal(int stack[], int *top, unsigned input);
+void change_base(char stack[], int *top, int base, unsigned input);
+int num_len, max_size;
 
 void main()
 {
-    printf("\nDecimal To Another Base Converter Using Stack\n");
+    printf("\nBase Converter Using Stack\n");
 
-    int top = -1, base_new, num_len, temp;
-    unsigned num; // to allow large inputs
+    int top = -1, base_new, temp;
+    unsigned long num; // to allow large inputs
 
     printf("\nEnter number: ");
     scanf("%u", &num);
     printf("Enter new base: ");
     scanf("%d", &base_new);
 
-    // printf("\n\n");
+    printf("\n");
     // num = 1234, base_new = 16; // 4D2
-    // (3735928559)2 -> (DEADBEEF)16
+    //                            // (3735928559)10 = (DEADBEEF)16
 
     num_len = num_strlen(num);
-    int stack[num_len * 10];
+    max_size = num_len * 10; // assumption
+    char stack[max_size];
 
-    switch (base_new)
+    if (base_new < 2 || base_new > 36)
     {
-    case 2:
-    case 3:
-    case 4:
-    case 5:
-    case 6:
-    case 7:
-    case 8:
-    case 9:
-        decimal_to_base_less_than_10(stack, &top, base_new, num);
-        printf("\n# %u converted to base %d:\n", num, base_new);
-        break;
-
-    case 16:
-        decimal_to_hexadecimal(stack, &top, num);
-        printf("\n# %u converted to base 16:\n", num);
-        break;
-
-    default:
-        printf("\n# Error: Invalid Base: valid bases are 2-9 and 16\n\n");
+        printf("\n# Error: Invalid Base\nValid bases are 2 to 36\n\n");
         exit(0);
     }
+    else
+        change_base(stack, &top, base_new, num);
+    printf("\n%u converted to base %d:\n", num, base_new);
 
     // printing base converted number
-    if (base_new == 16)
+    temp = pop(stack, &top);
+    while (temp != -99999)
     {
+        printf("%c", temp);
         temp = pop(stack, &top);
-        while (temp != -99999)
-        {
-            printf("%c", (char)temp);
-            temp = pop(stack, &top);
-        }
-        printf("\n");
-    }
-    else
-    {
-        temp = pop(stack, &top);
-        while (temp != -99999)
-        {
-            printf("%d", temp);
-            temp = pop(stack, &top);
-        }
-        printf("\n");
     }
 
-    printf("\n");
+    printf("\n\n");
 }
 
-int push(int stack[], int *top, int item)
+int push(char stack[], int *top, int item)
 {
-    if (*top == exp_len - 1) // stack overflow
+
+    if (*top == max_size - 1) // stack overflow
         return 0;
     stack[++*top] = item;
     return 1;
 }
 
-int pop(int stack[], int *top)
+int pop(char stack[], int *top)
 {
     if (*top == -1) // stack underflow
         return -99999;
@@ -104,54 +74,19 @@ int num_strlen(int input)
     return len;
 }
 
-void decimal_to_base_less_than_10(int stack[], int *top, int base, unsigned input)
+void change_base(char stack[], int *top, int base, unsigned input)
 {
+    unsigned long num;
+    char character;
     do
     {
-        push(stack, top, input % base);
+        num = input % base;
+
+        if (num >= 0 && num <= 9)
+            character = num + '0';
+        else if (num >= 10 && num <= 36)
+            character = num - 10 + 'A';
+
+        push(stack, top, character);
     } while (input /= base);
-}
-
-void decimal_to_octal(int stack[], int *top, unsigned input)
-{
-    do
-    {
-        push(stack, top, input % 8);
-    } while (input /= 8);
-}
-
-void decimal_to_hexadecimal(int stack[], int *top, unsigned input)
-{
-    unsigned hex_int;
-    char hex_char;
-    do
-    {
-        hex_int = input % 16;
-        switch (hex_int)
-        {
-        case 10:
-        case 11:
-        case 12:
-        case 13:
-        case 14:
-        case 15:
-            hex_char = hex_int - 10 + 'A';
-            break;
-        case 0:
-        case 1:
-        case 2:
-        case 3:
-        case 4:
-        case 5:
-        case 6:
-        case 7:
-        case 8:
-        case 9:
-            hex_char = (char)(hex_int + '0');
-            break;
-        default:
-            printf("\n# decimal_to_hexadecimal() error: hex_int: %d\n", hex_int);
-        }
-        push(stack, top, (int)hex_char);
-    } while (input /= 16);
 }
