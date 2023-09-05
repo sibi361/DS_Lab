21b81daff14a7e7ff8018dcc5f03bf467110f596
sibi361 <104310887+sibi361@users.noreply.github.com>
2023-09-05 12:22:06 +0530

    Add `lab_5_polish_notation/`

---
+++ lab_5_polish_notation/1_polish_convert_infix_postfix.c
@@ -0,0 +1,138 @@
+// Polish Notation
+// Conversion of infix expression to reverse Polish i.e.
+// postfix notation
+
+#include <stdio.h>
+#include <string.h>
+
+int max_size;
+
+int push(char stack[], int *top, char item);
+char pop(char stack[], int *top);
+int input_precedence(char literal);
+int output_precedence(char literal);
+
+void print_stack(char stack[], int top)
+{
+    printf("stack: ");
+    for (int i = 0; i <= top; i++)
+        printf("%c ", stack[i]);
+    printf("\n");
+}
+
+void main()
+{
+    // char exp[1000]; // input infix expression
+    // printf("\nEnter infix expression: ");
+    // scanf("%[^\n]s", exp);
+
+    // char exp[] = "(a+(b-c)*d)"; // abc-d*+
+    char exp[] = "a * b - c"; // ab*c-
+
+    max_size = strlen(exp);
+    char exp_postfix[max_size], temp, stack_top;
+    char stack[max_size];
+    int top, i, j;
+
+    top = i = j = 0;
+    stack[0] = '#'; // placeholder to indicate empty stack
+
+    while ((temp = exp[i++]) != '\0')
+    {
+        if (temp == ' ') // skip spaces
+            continue;
+
+        stack_top = pop(stack, &top);
+
+        // // debug
+        // puts(exp_postfix);
+        // printf("stack_top: %c | temp:  %c | top: %d\n", stack_top, temp, top);
+        // print_stack(stack, top);
+        // printf("\n");
+
+        if (output_precedence(stack_top) > input_precedence(temp))
+        {
+            exp_postfix[j++] = stack_top;
+            i--; // make sure to compare current literal on next iteration
+            continue;
+        }
+        else if (output_precedence(stack_top) == input_precedence(temp))
+            ;
+        else
+        {
+            push(stack, &top, stack_top);
+            push(stack, &top, temp);
+        }
+    }
+
+    while (top != 0) // exclude '#'
+        exp_postfix[j++] = pop(stack, &top);
+
+    exp_postfix[j] = '\0';
+    printf("\nObtained postfix expression:\n%s\n\n", exp_postfix);
+
+    // print_stack(stack, top);
+}
+
+int push(char stack[], int *top, char item)
+{
+    if (*top == max_size - 1) // stack overflow
+        return 0;
+    stack[++*top] = item;
+    return 1;
+}
+
+char pop(char stack[], int *top)
+{
+    if (*top == -1) // stack underflow
+        return '\0';
+    // prefix and postfix ops have higher precedence
+    // than dereference operator
+    return stack[(*top)--];
+}
+
+int input_precedence(char literal)
+{
+    switch (literal)
+    {
+    case ')':
+        return 0;
+    case '+':
+    case '-':
+        return 1;
+    case '%':
+    case '/':
+    case '*':
+        return 3;
+    case '^':
+        return 6;
+    case '(':
+        return 9;
+    default:
+        return 7;
+    }
+}
+
+int output_precedence(char literal)
+{
+    switch (literal)
+    {
+    case '#': // placeholder to indicate empty stack
+        return -1;
+    case '(':
+        return 0;
+    case '+':
+    case '-':
+        return 2;
+    case '%':
+    case '/':
+    case '*':
+        return 4;
+    case '^':
+        return 5;
+    case ')':
+        return 100;
+    default:
+        return 8;
+    }
+}
---
+++ lab_5_polish_notation/2_polish_evaluate_postfix.c
@@ -0,0 +1,121 @@
+/*
+Polish Notation
+Evaluation of reverse Polish i.e. postfix expression
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_SIZE 1000
+
+int eval_postfix(char exp[]);
+int is_operator(char operator);
+int push(int stack[], int *top, int item);
+int pop(int stack[], int *top);
+
+void main()
+{
+    // char exp[MAX_SIZE];
+    // printf("\nEnter postfix expression: ");
+    // scanf("%[^\n]s", exp);
+
+    // test cases
+    // 9-((8/2)*(2+3)) = -11
+    // 63-45+* = 27
+    char exp[MAX_SIZE] = "982/ 23+*-";
+
+    printf("\nResult = %d\n\n", eval_postfix(exp));
+}
+
+int eval_postfix(char exp[])
+{
+    char temp, op1, op2;
+    int stack[MAX_SIZE];
+    int top = -1, i = 0, result = 0;
+
+    while ((temp = exp[i++]) != '\0')
+    {
+        if (temp == ' ')
+            continue;
+        else if (!is_operator(temp))
+        {
+            // temp - '0' is converted character number to integer
+            if (!push(stack, &top, temp - '0'))
+            {
+                printf("Stack Overflow\n");
+                exit(0);
+            }
+        }
+        else
+        {
+            op2 = pop(stack, &top); // order is important
+            op1 = pop(stack, &top);
+
+            switch (temp)
+            {
+            case '^':
+                result = op1 ^ op2;
+                break;
+            case '%':
+                result = op1 % op2;
+                break;
+            case '/':
+                result = op1 / op2;
+                break;
+            case '*':
+                result = op1 * op2;
+                break;
+            case '+':
+                result = op1 + op2;
+                break;
+            case '-':
+                result = op1 - op2;
+                break;
+            default:
+                printf("Invalid operator: %c\n", temp);
+                exit(0);
+            }
+
+            // push result to stack
+            if (!push(stack, &top, result))
+            {
+                printf("Stack Overflow\n");
+                exit(0);
+            }
+        }
+    }
+    return result;
+}
+
+int is_operator(char operator)
+{
+    switch (operator)
+    {
+    case '^':
+    case '%':
+    case '/':
+    case '*':
+    case '+':
+    case '-':
+        return 1;
+    }
+    return 0;
+}
+
+int push(int stack[], int *top, int item)
+{
+    if (*top == MAX_SIZE - 1) // stack overflow
+        return 0;
+    stack[++*top] = item;
+    return 1;
+}
+
+int pop(int stack[], int *top)
+{
+    if (*top == -1) // stack underflow
+        return -99999;
+
+    // prefix and postfix ops have higher precedence
+    // than dereference operator
+    return stack[(*top)--];
+}
